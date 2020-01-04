d90a30f2610bf510604150ddfa554a4a4b0abe6f
grefer <hedro0458@gmail.com>
2020-01-04 13:52:20 +0900

    Day 3

--- Day 2/4.py
+++ Day 2/4.py
@@ -1,5 +1,5 @@
 s = '파이썬'
 
-print(s.encode()) # 문자열 암호화
+print(s.encode())  # 문자열 암호화. "파이썬" 을 암호화 하면 "b'\xed\x8c\x8c\xec\x9d\xb4\xec\x8d\xac'"
 
 print(b'\xed\x8c\x8c\xec\x9d\xb4\xec\x8d\xac'.decode()) # 문자열 복호화
