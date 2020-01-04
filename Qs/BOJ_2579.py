1a32d9544badeec12823bbe4f048e5a5c40db128
grefer <hedro0458@gmail.com>
2020-01-04 20:50:51 +0900

    2579 해결완료

--- Qs/BOJ_2579.py
+++ Qs/BOJ_2579.py
@@ -1,15 +1,23 @@
 n_of_stairs = int(input())
 
-scores = []
-mxs = [-1 for _ in range(n_of_stairs + 3)]
-
-for i in range(0, n_of_stairs):
-    scores.append(int(input()))
-mxs[0] = scores[0]
-mxs[1] = scores[0] + scores[1]
-mxs[2] = max(mxs[0] + scores[2], mxs[1] + scores[2])
-
-for i in range(3, n_of_stairs):
-    mxs[i] = max(mxs[i - 3] + scores[i - 1] + scores[i], mxs[i - 2] + scores[i])
+def solve(stairs):
+    score = []
+    max_score = [-1 for _ in range(stairs)]
     
-print(mxs[n_of_stairs - 1])
+    for i in range(stairs):
+        score.append(int(input()))
+    
+    if stairs == 1: return score[0]
+    if stairs == 2: return score[0] + score[1]
+    if stairs == 3: return max(score[0] + score[2], score[1] + score[2])
+
+    max_score[0] = score[0]
+    max_score[1] = score[0] + score[1]
+    max_score[2] = max(score[0] + score[2], score[1] + score[2])
+    
+    for i in range(3, stairs):
+        max_score[i] = max(max_score[i - 3] + score[i - 1] + score[i], max_score[i - 2] + score[i])
+
+    return max_score[stairs - 1]
+    
+print(solve(n_of_stairs))
