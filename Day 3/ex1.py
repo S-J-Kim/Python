0b9091dbcc407b64a6a60795c4f3096e204faae0
grefer <hedro0458@gmail.com>
2020-01-04 14:17:27 +0900

    강의록 추가

---
+++ Day 3/ex1.py
@@ -0,0 +1,9 @@
+print("안녕하세요")
+
+name = input("이름이 어떻게 되시나요? ")
+print(f'{name}님 만나서 반갑습니다.')
+
+print(f'이름의 길이는 다음과 같군요 : {len(name)}')
+
+age = int(input('나이는 어떻게 되시나요? '))
+print(f'내년이면 {age+1}살이 되시는군요.')
