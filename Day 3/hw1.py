bbeae3fa0eee81f60921253828f170728a95c5e7
grefer <hedro0458@gmail.com>
2020-01-05 13:35:23 +0900

    hw1 추가

--- Day 3/hw1.py
+++ Day 3/hw1.py
@@ -1,13 +1,13 @@
 grade = float(input())
 
 
-
-if 0 <= grade < 0.5 : print("")
-if 0.5 <= grade < : print("")
-if 1.0 <= grade < 1 : print("")
-if 1.75 <= grade < 1 : print("")
-if 2.3 <= grade < 1 : print("")
-if 2.8 <= grade < 1 : print("")
-if 3.5 <= grade < 1 : print("")
-if 4.2 <= grade < 1 : print("")
-if 4.5 == grade : print("")
+if grade==0:print("시대를 앞서가는 혁명의 씨앗")
+if 0 <= grade < 0.5 : print("플랑크톤")
+if 0.5 <= grade < 1.0: print("자벌레")
+if 1.0 <= grade < 1.75 : print("불가촉천민")
+if 1.75 <= grade < 2.3 : print("오락문화의 선구자")
+if 2.3 <= grade < 2.8 : print("일탈을 꿈꾸는 소시민")
+if 2.8 <= grade < 3.5 : print("일반인")
+if 3.5 <= grade < 4.2 : print("현 체제의 수호자")
+if 4.2 <= grade < 4.5: print("교수님의 사랑")
+if 4.5 == grade : print("신")
