ca33a52c4a822006283930ff9d1b498a182c9c0f
grefer <hedro0458@gmail.com>
2020-01-11 15:56:36 +0900

    이름좀 바꿨음

--- Day 5/숫자 찾기 게임.py
+++ Day 5/숫자 찾기 게임.py
@@ -1,12 +1,10 @@
 import random
 
-random_number = random.randint(1, 100)
+random_number = random.randint(1, 10000)
 count = 0
 
-user_input = ''
-
 while True:
-    user_input = input("1~100 사이의 숫자 입력 : ")
+    user_input = input("1~10000 사이의 숫자 입력 : ")
     
     if int(user_input) < random_number: print("UP"); count += 1
     elif int(user_input) > random_number: print("DOWN"); count += 1
