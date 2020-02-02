223f7926ad2adf2aa7fbdaa4cf961bd5650ede66
grefer <hedro0458@gmail.com>
2020-02-02 17:57:14 +0900

    Day 10

--- Day 9/예제/opinet.py
+++ Day 9/예제/opinet.py
@@ -3,4 +3,4 @@ from selenium import webdriver
 driver = webdriver.Chrome(r'Day 9\예제\data\chromedriver.exe')
 
 driver.get("http://www.opinet.co.kr")
-driver.find_element_by_css_selector("#gnb_0_0").click()
+driver.get('http://www.opinet.co.kr/searRgSelect.do')
