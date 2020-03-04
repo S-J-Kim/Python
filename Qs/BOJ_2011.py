c90557903359c846a6de14ed4ed95925c8a2e710
grefer <hedro0458@gmail.com>
2020-03-04 14:53:16 +0900

    studies

--- Qs/BOJ_2011.py
+++ Qs/BOJ_2011.py
@@ -7,27 +7,31 @@ def solve():
 
     dp = [1 for _ in range(len(code) + 1)]
 
+    if code[0] == 0: return 0
+    
     dp[1] = 1
 
     for i in range(2, len(code) + 1):
-        num = code[i - 2] * 10 + code[i - 1]
 
-        if 1 <= num <= 26:
-            if not num % 10 or num < 10:
+        num = code[i - 2] * 10 + code[i - 1]
+        
+        if not num: return 0
+
+        if 1 <= num < 10:
+            dp[i] = dp[i - 3]
+
+        elif 10 <= num <= 26:
+            if not num % 10:
                 dp[i] = dp[i - 2]
-            
             else:
                 dp[i] = dp[i - 1] + dp[i - 2]
-                  
-        elif num > 26:
-            if not num % 10:
-                return 0
-            else: dp[i] = dp[i - 1]
             
         else:
-            return 0
-
+            if not num % 10:
+                return 0
+            else:
+                dp[i] = dp[i - 1]
+        
     return dp[len(code)]
     
-print(solve())
-
+print(solve() % 1000000)
